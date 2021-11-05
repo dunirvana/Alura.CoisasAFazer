027acc0b3cb3b1736a6e305d718706bda3e449a8
Eduardo Teixeira <dunirvana@gmail.com>
2021-11-05 15:23:12 -0300

    Testes de integração

--- src/Alura.CoisasAFazer.WebApp/Controllers/TarefasController.cs
+++ src/Alura.CoisasAFazer.WebApp/Controllers/TarefasController.cs
@@ -2,6 +2,7 @@
 using Alura.CoisasAFazer.WebApp.Models;
 using Alura.CoisasAFazer.Core.Commands;
 using Alura.CoisasAFazer.Services.Handlers;
+using Alura.CoisasAFazer.Infrastructure;
 
 namespace Alura.CoisasAFazer.WebApp.Controllers
 {
@@ -9,18 +10,25 @@ namespace Alura.CoisasAFazer.WebApp.Controllers
     [ApiController]
     public class TarefasController : ControllerBase
     {
+        private readonly IRepositorioTarefas _repo;
+
+        public TarefasController(IRepositorioTarefas repo)
+        {
+            _repo = repo;
+        }
+
         [HttpPost]
         public IActionResult EndpointCadastraTarefa(CadastraTarefaVM model)
         {
             var cmdObtemCateg = new ObtemCategoriaPorId(model.IdCategoria);
-            var categoria = new ObtemCategoriaPorIdHandler().Execute(cmdObtemCateg);
+            var categoria = new ObtemCategoriaPorIdHandler(_repo).Execute(cmdObtemCateg);
             if (categoria == null)
             {
                 return NotFound("Categoria não encontrada");
             }
 
             var comando = new CadastraTarefa(model.Titulo, categoria, model.Prazo);
-            var handler = new CadastraTarefaHandler();
+            var handler = new CadastraTarefaHandler(_repo);
             handler.Execute(comando);
             return Ok();
         }
---
+++ tests/AluraCoisasAFazerTestes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AluraCoisasAFazerTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
